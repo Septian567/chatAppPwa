b48a0503ffe0dd753b133e47c9678005824bce1c
Septian Dika Maulana <99572215+Septian567@users.noreply.github.com>
2025-10-06 15:47:41 +0700

    Update apiConfig.ts

--- app/utils/apiConfig.ts
+++ app/utils/apiConfig.ts
@@ -1,3 +1,1 @@
-// utils/apiConfig.ts
-export const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://52.77.220.48:5000";
-// export const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+export const BASE_URL = process.env.NEXT_PUBLIC_API_URL || "https://chatapi.ddns.net/";
